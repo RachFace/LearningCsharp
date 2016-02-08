40b06cd344c5f0af6de20a882d704517363366a5
Rachael <91rachface@gmail.com>
2016-02-08 22:13:45 +0000

    Can now calculate a person year of birth from age and error handling

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -8,17 +8,37 @@ namespace Practice1
 {
     class Program
     {
+        
         static void Main(string[] args)
 
         {
             Console.WriteLine("Please enter your name"); 
-            String Name = Console.ReadLine();
-            Console.WriteLine("Your name is " + Name);
+            String newdate = Console.ReadLine();
+            Console.WriteLine("Your name is " + newdate);
+            Console.ReadKey();
+
+            Console.WriteLine("Please enter your age");
+            var input = Console.ReadLine();
+            int num1;
+            var MyAge = Int32.TryParse(input, out num1);
+            if (MyAge == true)
+            {
+                // The input was good and we have a new number in num1
+            }
+            else
+            {
+                // the input was bad and the user should feel bad
+            }
+            var myDate = DateTime.Now;
+            var newDate = myDate.AddYears(- num1);
+            Console.WriteLine("Your birth year is " + newDate.Year);
             Console.ReadKey();
 
             string valueString = "Hello World";
             Console.WriteLine(valueString);
             Console.ReadKey();
         }
+
+
     }
 }
