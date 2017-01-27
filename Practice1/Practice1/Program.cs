204810616295c4147c420cad59af70627f0f8f71
Rachael <91rachface@gmail.com>
2017-01-27 21:45:41 +0000

    Revert "Will now loop question until "quit" is typed"
    This reverts commit 30aff10a71ee2bd57859326e5664c81dc1a35175.

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -8,14 +8,37 @@ namespace Practice1
 {
     class Program
     {
-
+        
         static void Main(string[] args)
 
         {
-            while (Console.ReadLine() != "quit")
-            Console.WriteLine("Please enter your name");
-            String name = Console.ReadLine();
-           }
+            Console.WriteLine("Please enter your name"); 
+            String newdate = Console.ReadLine();
+            Console.WriteLine("Your name is " + newdate);
+            Console.ReadKey();
 
+            Console.WriteLine("Please enter your age");
+            var input = Console.ReadLine();
+            int num1;
+            var MyAge = Int32.TryParse(input, out num1);
+            if (MyAge == true)
+            {
+                // The input was good and we have a new number in num1
+            }
+            else
+            {
+                // the input was bad and the user should feel bad
+            }
+            var myDate = DateTime.Now;
+            var newDate = myDate.AddYears(- num1);
+            Console.WriteLine("Your birth year is " + newDate.Year);
+            Console.ReadKey();
+
+            string valueString = "Hello World";
+            Console.WriteLine(valueString);
+            Console.ReadKey();
         }
+
+
     }
+}
